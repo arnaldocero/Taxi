94b4f34fde84613200d0ccea9b496dec21e45cb8
Arnaldo Alfonso Pacheco Sarmiento <p.arnaldoalfonso@hotmail.com>
2020-08-11 11:50:39 -0500

    Archivos de lenguajes

--- Taxi.Prism/Taxi.Prism/Resources/Resource.Designer.cs
+++ Taxi.Prism/Taxi.Prism/Resources/Resource.Designer.cs
@@ -22,7 +22,7 @@ namespace Taxi.Prism.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resource {
+    public class Resource {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace Taxi.Prism.Resources {
         ///   Devuelve la instancia de ResourceManager almacenada en caché utilizada por esta clase.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Taxi.Prism.Resources.Resource", typeof(Resource).Assembly);
@@ -51,12 +51,66 @@ namespace Taxi.Prism.Resources {
         ///   búsquedas de recursos mediante esta clase de recurso fuertemente tipado.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Accept.
+        /// </summary>
+        public static string Accept {
+            get {
+                return ResourceManager.GetString("Accept", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Check the internet connection..
+        /// </summary>
+        public static string ConnectionError {
+            get {
+                return ResourceManager.GetString("ConnectionError", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Error.
+        /// </summary>
+        public static string Error {
+            get {
+                return ResourceManager.GetString("Error", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a You must enter a plaque..
+        /// </summary>
+        public static string PlaqueError1 {
+            get {
+                return ResourceManager.GetString("PlaqueError1", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a The plaque must start with three letters and end with three numbers..
+        /// </summary>
+        public static string PlaqueError2 {
+            get {
+                return ResourceManager.GetString("PlaqueError2", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Taxi History.
+        /// </summary>
+        public static string TaxiHistory {
+            get {
+                return ResourceManager.GetString("TaxiHistory", resourceCulture);
             }
         }
     }
